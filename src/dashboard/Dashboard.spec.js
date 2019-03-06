361fdae23ce31e0c41903b79d03e9438a78f6aac
Daniel Weinman <namniewd@gmail.com>
2019-03-06 16:20:51 -0500

    wrote tests to ensure the correct functionality of the Dashboard component

--- src/dashboard/Dashboard.spec.js
+++ src/dashboard/Dashboard.spec.js
@@ -9,7 +9,7 @@ import Dashboard from './Dashboard';
 
 describe('<Dashboard />', () => {
     describe('Lock/Unlock Button functionality', () => {
-        it('lock button should lock gate if gate is closed', () => {
+        it('lock button should lock gate if gate is closed and unlocked', () => {
             const { getByText } = render(<Dashboard />)
             const closeBtn = getByText(/close gate/i);
             const lockBtn = getByText(/Lock Gate/i);
@@ -21,9 +21,45 @@ describe('<Dashboard />', () => {
             fireEvent.click(lockBtn);
             expect(locked).toHaveClass('red-led');
         });
+
+        it('lock button should unlock gate if gate is closed and locked', () => {
+            const { getByText } = render(<Dashboard />)
+            const closeBtn = getByText(/close gate/i);
+            const lockBtn = getByText(/Lock Gate/i);
+            // close the gate to enable lock button for testing
+            fireEvent.click(closeBtn);
+            const locked = getByText(/Unlocked/);
+            // expect display to show unlocked now
+            expect(locked).toHaveClass('green-led');
+            // first click locks
+            fireEvent.click(lockBtn);
+            // second click unlocks
+            fireEvent.click(lockBtn);
+            expect(locked).toHaveClass('green-led');
+        });
     });
 
     describe('Open/Close Button functionality', () => {
-        
+        it('should open gate if gate is closed and unlocked', () => {
+            const { getByText } = render(<Dashboard />);
+            const button = getByText(/close gate/i);
+            fireEvent.click(button);
+            const closed = getByText(/closed/i);
+            expect(closed).toHaveClass('red-led');
+        });
+
+        it('should close gate if gate is open and unlocked', () => {
+            const { getByText } = render(<Dashboard />);
+            const button = getByText(/close gate/i);
+            // first click should closed gate
+            fireEvent.click(button);
+            const closed = getByText(/closed/i);
+            // check if gate is closed
+            expect(closed).toHaveClass('red-led');
+            // second click will toggle gate back to open
+            fireEvent.click(button);
+            // check if gate is open
+            expect(closed).toHaveClass('green-led');
+        });
     });
 });
